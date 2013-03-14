fd66c02d0ddab29997972c4d8730eb892bbdda67
jminardi <jminardi@enthought.com>
2013-03-13 21:43:03 -0500

    implement click on links to delete them

--- sensor_client.py
+++ sensor_client.py
@@ -2,6 +2,7 @@
 import threading
 
 import numpy as np
+from numpy.linalg import norm
 import enaml
 #import zmq
 from enable.api import Component
@@ -43,14 +44,24 @@ N_INPUTS = 6
 N_OUTPUTS = 3
 
 
+def is_close_to_one(number, tolerence=.001):
+    return True if abs(1 - number) < tolerence else False
+
+
 class ConnectionsComponent(Component):
 
     # List of lines to draw
     lines = List()
 
+    # Distance from edge to draw the endpoints
     b = Int(30)
 
+    # Radius of the endpoints
     r = Int(15)
+
+    input_indexes = List()
+
+    output_indexes = List()
 
     bgcolor = (0.9294, 0.9294, 0.9294)
 
@@ -63,10 +74,10 @@ class ConnectionsComponent(Component):
 
     def _draw_endpoints(self, gc):
         self.w, self.h = w, h = gc.width(), gc.height()
-        gc.set_stroke_color((37/255., 51/255., 112/255., 1))
+        gc.set_stroke_color((0.145, 0.2, 0.439, 1))
         gc.set_line_width(4.0)
-        self.r = r = 15
-        self.b = b = 30
+        r = self.r
+        b = self.b
         s = h / 13
         self.left_circle_pos = lcp = np.linspace(s, h - s, N_INPUTS)
         self.right_circle_pos = rcp = lcp[:N_OUTPUTS]
@@ -79,34 +90,40 @@ class ConnectionsComponent(Component):
     def _draw_lines(self, gc):
         w, h = gc.width(), gc.height()
         gc.set_line_width(4.0)
-        gc.set_stroke_color((172/255., 175/255., 179/255., 1))
+        gc.set_stroke_color((0.675, 0.686, 0.702, 1))
         for line in self.lines:
             x1, y1, x2, y2 = line
             gc.move_to(x1, y1)
             gc.line_to(x2, y2)
         gc.stroke_path()
 
+    ### 'normal' Event State Handlers  ##########################################
+
     def normal_key_pressed(self, event):
         print "key pressed: ", event.character
 
     def normal_left_down(self, event):
-        circle_y = self._get_nearest_circle(event.x, event.y)
+        circle_y, i = self._get_nearest_circle(event.x, event.y)
+        # if we hit an input circle, start a new link line
         if circle_y is not None:
             self.lines.append([self.b, circle_y, event.x, event.y])
+            self.input_indexes.append(i)
             self.event_state = 'connecting'
             event.handled = True
             self.request_redraw()
-        else:
-            pass
-            # XXX Implement this
-            #clicked_line = self._get_nearest_line(self, x, y)
-            #if clicked_line:
-            #    self.lines.pop(clicked_line)
+        else:  # if we didn't git a circle, see if we hit a line then remove it
+            clicked_line_index = self._get_nearest_line(event.x, event.y)
+            if clicked_line_index is not None:
+                self.lines.pop(clicked_line_index)
+                self.request_redraw()
+
+    ### 'connecting' Event State Handlers  ##########################################
 
     def connecting_left_down(self, event):
-        circle_y = self._get_nearest_circle(event.x, event.y)
+        circle_y, i = self._get_nearest_circle(event.x, event.y)
         if circle_y:
             self.lines[-1][2:] = [self.w - self.b, circle_y]
+            self.output_indexes.append(i)
         else:
             self.lines.pop()
         self.event_state = 'normal'
@@ -118,28 +135,46 @@ class ConnectionsComponent(Component):
         event.handled = True
         self.request_redraw()
 
-    def _get_nearest_circle(self, x, y):
-        """ Returns the nearest circle or None if there are no circles within
-            `distance` of x and y.
+    ### Private methods  ######################################################
 
+    def _get_nearest_circle(self, x, y):
+        """ Returns the nearest circle and index or None.
         """
         if self.event_state == 'normal':  # only check input circles
             left_gutter = self.b + self.r
             if x > left_gutter:  # border + radius
-                return None
-            for circle_y in self.left_circle_pos:
+                return None, 0
+            for i, circle_y in enumerate(self.left_circle_pos):
                 if circle_y - self.r < y and circle_y + self.r > y:
-                    return circle_y
-            return None
+                    return circle_y, i
+            return None, 0
 
         if self.event_state == 'connecting':  # only check output cirlces
             right_gutter = self.w - (self.b + self.r)
             if x < right_gutter:
-                return None
-            for circle_y in self.right_circle_pos:
+                return None, 0
+            for i, circle_y in enumerate(self.right_circle_pos):
                 if circle_y - self.r < y and circle_y + self.r > y:
-                    return circle_y
+                    return circle_y, i
+            return None, 0
+
+    def _get_nearest_line(self, x, y):
+        """ Returns the nearest line index, or None.
+        """
+
+        if not (self.b + self.r < x < self.w - self.b):  # In a gutter
             return None
+
+        for i, line in enumerate(self.lines):
+            # Check to see if a line drawns from each lines start point is
+            # parallel to that line. If it is, then we hit the line.
+            orig_x, orig_y = line[:2]
+            line_x, line_y = line[2:]
+            line = np.array([line_x - orig_x, line_y - orig_y])
+            click = np.array([x - orig_x, y - orig_y])
+            angle = np.dot(line, click) / norm(line) / norm(click)
+            if is_close_to_one(angle):
+                return i
 
 
 class IOController(HasStrictTraits):
